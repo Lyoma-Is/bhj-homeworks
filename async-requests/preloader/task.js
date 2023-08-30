2e9b3e82d3cb3c63c07b6e6e9c2f0242fe17ab73
Lyoma <140175640+Lyoma-Is@users.noreply.github.com>
2023-08-30 21:11:10 +0300

    732

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,33 +1,32 @@
-'use strict';
+const xhr = new XMLHttpRequest();
+const listCurrency = document.getElementById('items');
+const loader = document.querySelector('.loader')
 
-let request = new XMLHttpRequest();
-let loader = document.getElementById('loader');
+const outputData = data => {
+	if (data.length === 0)
+		return;
+	let itemsHTML = '';
+	for (const item of data) {
+		itemsHTML += `<div class="item"><div class="item__code">${item.name}</div><div class="item__value">${item.value}</div><div class="item__currency">руб.</div></div>`;
+		listCurrency.innerHTML = itemsHTML;
+	}
+	loader.classList.remove('loader_active');
+}
 
-function insertHTML(response) {
-    let itemList = document.querySelectorAll('div.item');
-    itemList.length > 0 ? Array.from(itemList).forEach((element) => element.remove()) : '';
-    for (let item in response) {
-        let html =
-            `<div class="item">
-                <div class="item__code">${response[item].CharCode}</div>
-                <div class="item__value">${response[item].Value}</div>
-                <div class="item__currency">руб.</div>
-            </div>`
-        document.getElementById('items').insertAdjacentHTML('afterbegin', html);
-    };
-    loader.classList.remove('loader_active');
-};
-
-function renderPage(response) { response.length > 0 ? insertHTML(JSON.parse(response)) : ''; };
-
-request.open('GET', 'https://netology-slow-rest.herokuapp.com', true);
-request.send();
-request.addEventListener('readystatechange', function () {
-    if (this.readyState == 4 && this.status == 200) {
-        let response = JSON.parse(this.responseText)['response'].Valute;
-        insertHTML(response);
-        localStorage.setItem('valute', JSON.stringify(response));
-    };
-});
-
-renderPage(localStorage.getItem('valute'));
+xhr.addEventListener('readystatechange', () => {
+	if (xhr.readyState !== xhr.DONE) {
+		loader.classList.add('loader_active')
+	} else {
+		const loadCurrency = JSON.parse(xhr.responseText).response.Valute;
+		const serverData = [];
+		for (const key in loadCurrency) {
+			serverData.push({
+				name: loadCurrency[key].CharCode,
+				value: loadCurrency[key].Value
+			});
+		}
+		outputData(serverData);
+	}
+})
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
+xhr.send();
